987f8e43390dc9888c7775e132b066b65fd07270
bryan-beach <bryan.beach.17@cnu.edu>
2020-02-11 17:25:32 -0500

    it compiles now

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -22,3 +22,49 @@
 
 //TODO look in utilities.h for useful functions, particularly strip_unwanted_chars!
 
+#include "array_functions.h"
+
+void clearArray(){
+	;
+}
+
+int getArraySize(){
+	return 0;
+}
+
+std::string getArrayWordAt(int i){
+	return " ";
+}
+int getArrayWord_NumbOccur_At(int i) {
+	return 0;
+}
+
+bool processFile(std::fstream &myfstream) {
+	return true;
+}
+
+void processLine(std::string &myString) {
+	;
+}
+
+void processToken(std::string &token) {
+
+};
+
+bool openFile(std::fstream& myfile, const std::string& myFileName,
+		std::ios_base::openmode mode) {
+		return true;
+		}
+
+void closeFile(std::fstream& myfile) {
+	;
+}
+
+int writeArraytoFile(const std::string &outputfilename) {
+	return 0;
+}
+
+void sortArray(constants::sortOrder so) {
+	;
+}
+
